25b8e71725833a75e3d178bf26219030f6545c80
Raajn Patel <rndpatel@gmail.com>
2019-08-12 02:23:37 -0700

    Step 4 complete added function to add points to the total score

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
-  const [homescore] = useState(0);
-  const [awayScore] = useState(0);
+  const [homeScore, homeAdd] = useState(0);
+  const [awayScore, awayAdd] = useState(0);
 
   return (
     <div className="container">
@@ -18,12 +18,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{homescore}</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:08</div>
           <div className="away">
             <h2 className="away__name">Celtics</h2>
-            <div className="away__score">{awayscore}</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -31,12 +31,14 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="home_threePoint" onClick={()=>homeAdd(homeScore+3)}>3 Points</button>
+          <button className="home_twoPoint" onClick={()=>homeAdd(homeScore+2)}>2 Pointer</button>
+            <button className="home_onePoint" onClick={()=>homeAdd(homeScore+1)}>Free Throw</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="away_threePoint" onClick={()=>awayAdd(awayScore+3)}>3 Pointer</button>
+          <button className="away_twoPoint" onClick={()=>awayAdd(awayScore+2)}>2 Pointer</button>
+            <button className="away_onePoint" onClick={()=>awayAdd(awayScore+1)}>Free Throw</button>
         </div>
       </section>
     </div>
